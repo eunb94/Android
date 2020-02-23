6df99aeb687299beb61c1acafd88ea4aa87e229e
이은비 <53929568+eunb94@users.noreply.github.com>
2020-02-23 22:08:35 +0900

    first commit

---
+++ java/com/example/application/Update.java
@@ -0,0 +1,100 @@
+package com.example.application;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class Update extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.update);
+        final Context _this = Update.this;
+        final String[] arr = this.getIntent()
+                .getStringExtra("spec")
+                .split(",");
+        /*
+        * member.seq,member.name,
+                                member.email,member.phone,
+                                member.addr,member.photo
+        * */
+        ImageView profile = findViewById(R.id.profile);
+        profile.setImageDrawable(
+                getResources()
+                        .getDrawable(
+                                getResources()
+                                        .getIdentifier(
+                                                _this.getPackageName()+":drawable/"+arr[5],
+                                                null,
+                                                null
+                                        )
+                        )
+        );
+        final TextView nameTV = findViewById(R.id.name);
+        final TextView emailTV = findViewById(R.id.changeEmail);
+        final TextView phoneTV = findViewById(R.id.changePhone);
+        final TextView addrTV = findViewById(R.id.changeAddress);
+        nameTV.setText(arr[1]);
+        emailTV.setText(arr[2]);
+        phoneTV.setText(arr[3]);
+        addrTV.setText(arr[4]);
+        findViewById(R.id.confirmBtn)
+                .setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        ItemUpdate query = new ItemUpdate(_this);
+                        query.name = nameTV.getText().toString();
+                        query.email = emailTV.getText().toString();
+                        query.phone = phoneTV.getText().toString();
+                        query.addr = addrTV.getText().toString();
+                        query.seq = arr[0];
+                        query.run();
+                        Intent intent = new Intent(_this, MemberDetail.class);
+                        intent.putExtra("seq", arr[0]);
+                        startActivity(intent);
+
+                    }
+                });
+    }
+    private class MemberUpdateQuery extends Main.QueryFactory{
+        SQLiteOpenHelper helper;
+        public MemberUpdateQuery(Context _this) {
+            super(_this);
+            helper = new Main.SQLiteHelper(_this);
+        }
+
+        @Override
+        public SQLiteDatabase getDatabase() {
+            return helper.getWritableDatabase();
+        }
+    }
+    private class ItemUpdate extends MemberUpdateQuery {
+        String name, email, phone, addr, seq;
+        public ItemUpdate(Context _this) {
+            super(_this);
+        }
+        public void run(){
+            String sql = String.format(
+                    " UPDATE %s\n" +
+                            "     SET %s = '%s',\n" +
+                            "         %s = '%s',\n" +
+                            "         %s = '%s',\n" +
+                            "         %s = '%s'\n" +
+                            "     WHERE %s LIKE '%s'",
+                    Main.MEMBERS, Main.NAME, name,
+                    Main.EMAIL, email,
+                    Main.PHONE, phone,
+                    Main.ADDR, addr, Main.SEQ, seq);
+            getDatabase().execSQL(sql);
+
+        }
+    }
+
+}
